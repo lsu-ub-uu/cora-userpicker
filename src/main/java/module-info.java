48c1b1e739ebcb2485f359ecfd3414d25c05c9a4
olovm <olov@mckie.se>
2019-03-21 07:59:23 +0000

    adding classic servicedefintion

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,7 @@
 module se.uu.ub.cora.userpicker {
-	requires se.uu.ub.cora.gatekeeper;
+	requires transitive se.uu.ub.cora.gatekeeper;
+
+	exports se.uu.ub.cora.userpicker;
 
 	provides se.uu.ub.cora.gatekeeper.user.UserPickerProvider
 			with se.uu.ub.cora.userpicker.UserInStorageUserPickerProvider;
